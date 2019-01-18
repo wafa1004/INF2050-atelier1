c0714ea04cd7d9dab8168179ff83a718a82e32f8
WafaIsm <ismailwafa95@gmail.com>
2019-01-18 15:11:04 -0500

    Add file factorial.

---
+++ factorial/src/factorial.java
@@ -0,0 +1,15 @@
+public class factorial
+{
+    public static void main(String[] args) {
+        System.out.println(fact(4));
+    }
+
+    public static int fact(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return(n * fact(n-1));
+
+    }
+
+}
