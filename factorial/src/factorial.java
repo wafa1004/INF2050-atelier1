734e547e58538c22c937359e760d17b5f83b6138
WafaIsm <ismailwafa95@gmail.com>
2019-01-18 15:16:33 -0500

    Change the value of factorial to 7.

--- factorial/src/factorial.java
+++ factorial/src/factorial.java
@@ -1,7 +1,7 @@
 public class factorial
 {
     public static void main(String[] args) {
-        System.out.println(fact(4));
+        System.out.println(fact(7));
     }
 
     public static int fact(int n) {
