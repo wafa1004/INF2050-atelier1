7f32233c9d92bca261e761345258563f430d677e
wafa1004 <ismailwafa1004@gmail.com>
2019-04-21 20:25:50 -0400

    Update main

--- factorial/src/factorial.java
+++ factorial/src/factorial.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 // This program calculates the factorial of a number.
 public class factorial
 {
     public static void main(String[] args) {
-        System.out.println(fact(7));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number you'd like to calculate its factorial then press enter: ");
+        int n = scanner.nextInt();
+        System.out.println("The factorial of the number "+n+" is : "+fact(n));
     }
 
     public static int fact(int n) {
@@ -10,8 +16,6 @@ public class factorial
             return 1;
         else
             return(n * fact(n-1));
-
     }
 
 }
-// This is a test comment.
